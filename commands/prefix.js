e5261ba279f905a3ad009725079aebf8d6c8731a
De-Snail <33108771+De-Snail@users.noreply.github.com>
2020-09-01 12:53:52 -0700

    Delete prefix.js

--- commands/prefix.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'prefix',
-  description: 'sends a list of all prefixes',
-  aliases: ['bots'],
-  execute(message, args) {
-    message.delete();
-    message.channel.send('Corona: c!\nRick: $\nRythm: ~\nRythm 2: >\nTatsu: !');
-  },
-};
