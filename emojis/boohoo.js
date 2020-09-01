19d3f7a186a46a9bc1dc42c4dd88941f4b3e3413
De-Snail <33108771+De-Snail@users.noreply.github.com>
2020-09-01 12:54:21 -0700

    Delete boohoo.js

--- emojis/boohoo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'boohoo',
-  execute(message, args) {
-    message.delete();
-    message.channel.send('https://cdn.discordapp.com/attachments/494298326066528276/748234415339077682/image0.jpg');
-  },
-};
