ac84580561b1a63e12845b3f44d415b102c48616
De-Snail <33108771+De-Snail@users.noreply.github.com>
2020-09-01 12:54:32 -0700

    Delete wall.js

--- emojis/wall.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'wall',
-  execute(message, args) {
-    message.delete();
-    message.channel.send('https://cdn.discordapp.com/attachments/528750818166439937/748575194657849394/image0.jpg');
-  },
-};
